38fba3b60be906d98f2dc5c14f227ab38c9ea367
Indumathy. <indumathychakkarapani@gmail.com.>
2025-11-06 14:36:22 +0530

    core java completed

---
+++ C2TC_CoreJavaCourse-master/Core_Java_Courses/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Core_Java_Courses {
+	requires java.sql;
+	requires mysql.connector.j;
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.params;
+	requires org.junit.platform.suite.api;
+}
